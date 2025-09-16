f25ed016f594b6003031e4b34313cf77a234c10b
mandyx07 <amanda.miranda3@estudante.ifms.edu.br>
2025-09-16 09:18:39 -0300

    Criando um proxy e um rewrites para rodar local e na vercel.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
-
+type EstudantesType = {
+  _id:string,
+  nome:string,
+  idade:number
+}
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() =>{
+    fetch("/api/estudantes")
+    .then((response)=> response.json())
+    .then((dados)=> setEstudantes(dados))
+  
+  },[])
+  const [estudantes, setEstudantes] = useState<EstudantesType[]>([])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <h1>Lista de Estudantes</h1>
+     <div className="conteiner-estudantes">
+      {
+        estudantes.map((estudante)=>{
+          return(
+            <div key={estudante._id}>
+              <h2>{estudante.nome}</h2>
+              <p> Idade: {estudante.idade}</p>
+            </div>
+          )
+        })
+      }
+     </div>
+    
+    
     </>
   )
 }
