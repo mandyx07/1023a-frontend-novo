3a4644bf968dca0ed95adde9a0b2d2af627d3f1e
mandyx07 <amanda.miranda3@estudante.ifms.edu.br>
2025-09-22 10:03:16 -0300

    atualizando o codigo do beck-end

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,40 @@ function App() {
   
   },[])
   const [estudantes, setEstudantes] = useState<EstudantesType[]>([])
-
+  const[nome, setNome] = useState("")
+  const[idade, setIdade] = useState(0)
+  function handleSubmit(e:React.FormEvent){
+    e.preventDefault ()
+    const estudante ={nome, idade}
+    fetch("/api/estudantes",{
+      method:"POST",
+      headers:{
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(estudante)
+    })
+    .then((response) => response.json())
+    .then((dados)=>{
+      setEstudantes([...estudantes, dados])
+      setNome("")
+      setIdade(0)
+    })
+  }
   return (
     <>
+    <h1>Cadastro de Estudantes</h1>
+    <form onSubmit={handleSubmit}>
+      <input type ='text' placeholder='nome' value ={nome}
+      onChange={(e)=> setNome(e.target.value)} />
+
+      <input type ="number" placeholder='idade' value ={idade}
+      onChange={(e)=> setIdade(Number(e.target.value))} />
+      
+      <button type='submit'>Cadastrar</button>
+    </form>
+
+
+
      <h1>Lista de Estudantes</h1>
      <div className="conteiner-estudantes">
       {
